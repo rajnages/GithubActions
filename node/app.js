166f9d9e7a132318a3d79e33bd6f3126bee9a9c4
Rajavarapu Nageswarao <nagesrajavarapu@gmail.com>
2024-11-23 09:55:52 +0530

    test cases are added

--- node/app.js
+++ node/app.js
@@ -1,11 +1,17 @@
+const request = require("supertest");
 const express = require("express");
-const app = express();
 
+const app = express();
 app.get("/", (req, res) => {
   res.send("Hello from Node.js deployed via GitHub Actions!");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+describe("GET /", () => {
+  it("responds with Hello message", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe(
+      "Hello from Node.js deployed via GitHub Actions!"
+    );
+  });
 });
